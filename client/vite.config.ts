f1ffdb17e664fe81cf0ae2dc6c3aac05d4bea738
Jacob Erickson <jacobroberterickson@gmail.com>
2024-12-03 14:16:15 -0700

    full working without extraneous parts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,12 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: './src/main.jsx',
+      input: './src/main.tsx',
+      output: {
+        entryFileNames: '[name].js', // Ensure output files are .js
+        chunkFileNames: '[name].js',  // Ensure chunk files are .js
+        assetFileNames: '[name].[ext]' // Maintain original asset file names
+      },
     },
     outDir: '../dist/static'
   },
